fba97f318602a1e84b5d1ac66cd0d65e2c39c5e8
Bhavik-20 <bbhatt2005@gmail.com>
2023-08-09 13:48:09 -0400

    Mongodb connection made

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,6 +1,6 @@
 import posts from "./tuits.js";
 let tuits = posts;
-
+import * as tuitsDao from './tuits-dao.js'
 
 const TuitsController = (app) => {
     app.post('/api/tuits', createTuit);
@@ -9,10 +9,9 @@ const TuitsController = (app) => {
     app.delete('/api/tuits/:tid', deleteTuit);
 }
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
   console.log(req.body);
   const newTuit = req.body;
-  newTuit._id = (new Date()).getTime()+'';
   newTuit.likes = 0;
   newTuit.liked = false;
   newTuit.dislikes = 0;
@@ -24,28 +23,28 @@ const createTuit = (req, res) => {
   newTuit.time = "2h";
   newTuit.replies = 0;
   newTuit.retuits = 0;
-  tuits.push(newTuit);
-  res.json(newTuit);
+  const insertedTuit = await tuitsDao.createTuit(newTuit);
+  res.json(insertedTuit);
 }
 
-const findTuits  = (req, res) => {
+const findTuits  = async (req, res) => {
+  const tuits = await tuitsDao.findTuits();
     return res.json(tuits);
 }
 
-const updateTuit = (req, res) => {
+const updateTuit = async (req, res) => {
   const tuitdId = req.params.tid;
   const updates = req.body;
-  console.log(updates);
-  const tuitIndex = tuits.findIndex((t) => t._id === tuitdId)
-  tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
-  res.sendStatus(200);
+  console.log(tuitdId, updates);
+  const status = await tuitsDao.updateTuit(tuitdId, updates);
+  res.json(status);
 }
 
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
   const tuitdIdToDelete = req.params.tid;
-  tuits = tuits.filter((t) =>
-    t._id !== tuitdIdToDelete);
-  res.sendStatus(200);
+  const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+  tuits = tuits.filter(t =>t._id !== tuitdIdToDelete);
+  res.json(status);
 }
 
 
